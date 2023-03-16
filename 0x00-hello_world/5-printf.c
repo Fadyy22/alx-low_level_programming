67612366de52b50491c8f1bd6cb28ad5e47e0d04
Fady <fadyalaa441@gmail.com>
2023-03-16 07:40:46 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * printf - Prints the string inside parentheses
+	 */
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
