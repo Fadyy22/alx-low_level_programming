e13d77e2b921386199788065bbae2fceda4213d7
Fady <fadyalaa441@gmail.com>
2023-03-16 07:32:58 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+	/**
+	 * puts - Prints the string inside parentheses followed by a new line
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
