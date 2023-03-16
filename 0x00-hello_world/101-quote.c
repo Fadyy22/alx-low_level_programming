4f3bf28ab4244d734e09a4884e7858dc123c8e76
Fady <fadyalaa441@gmail.com>
2023-03-16 08:34:43 -0700

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1
+ */
+int main(void)
+{
+	/*
+	 * printf - Prints the string inside parentheses
+	 */
+	printf("and that piece of art is useful %c  - Dora Korpar, 2015-10-19%c", '"', '\n');
+	return (1);
+}
