c61c56c475f05691d0ec7e51584a73df8b0b5c2a
Fady <fadyalaa441@gmail.com>
2023-03-17 07:19:45 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c = c - 1;
+	}
+	putchar('\n');
+	return (0);
+}
