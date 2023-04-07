d97fe6dec99431153cf0920b7fa25dd575fb9ad1
Fady <fadyalaa441@gmail.com>
2023-04-07 11:33:02 -0700

    Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _pow_recursion - returns the value of x raised to the power of y
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
  *
  * @x: int (base)
  * @y: int (power)
