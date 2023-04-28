a2348ac5a3933acbf495a5d48bca567fbee1a2ba
Fadyy22 <fadyalaa441@gmail.com>
2023-04-28 07:34:56 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,19 +9,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	listint_t *temp;
 	size_t no_nodes;
 
-	temp = h;
 	no_nodes = 0;
 
-	if (temp == NULL)
-		return (NULL);
-
-	while (temp != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n)", temp->n);
-		temp = temp->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		no_nodes++;
 	}
 
