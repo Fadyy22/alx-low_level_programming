996733246ec47021a2dc5a0cac30966515fa10bf
Fadyy22 <fadyalaa441@gmail.com>
2023-04-29 15:20:55 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,11 +32,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+
 	if (temp == NULL && idx != 0)
 		return (NULL);
-
-	for (i = 0; i < idx - 1; i++)
-		temp = temp->next;
 
 	new_node->next = temp->next;
 	temp->next = new_node;
