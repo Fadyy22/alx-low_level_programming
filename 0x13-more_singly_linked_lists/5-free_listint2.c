516a40f182a77167768ce35896785e7f934a0a22
Fadyy22 <fadyalaa441@gmail.com>
2023-04-29 11:29:16 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 
-	while (temp != NULL)
+	if (head != NULL)
 	{
-		temp = temp->next;
-		free(*head);
-		*head = temp;
+		while (temp != NULL)
+		{
+			temp = temp->next;
+			free(*head);
+			*head = temp;
+		}
 	}
-	free(head);
 }
