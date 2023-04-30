0a66bc899c40f24389f84575d949ebeef2cb2cd7
Fadyy22 <fadyalaa441@gmail.com>
2023-04-30 15:04:08 -0700

    Reverse list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,8 +9,10 @@
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
+	listint_t *prev, *next;
+
+	prev = NULL;
+	next = NULL;
 
 	while (*head != NULL)
 	{
