1943cf486e3cf7f011b2afabb66197f2410a05ac
Fadyy22 <fadyalaa441@gmail.com>
2023-05-08 16:10:14 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -31,7 +31,7 @@ int create_file(const char *filename, char *text_content)
 	if (!text_content)
 		text_content = "";
 
-	for (no_letters = 0; textcontent[no_letters] != '\0'; no_letters++)
+	for (no_letters = 0; text_content[no_letters] != '\0'; no_letters++)
 		;
 
 	success_write = write(file_d, text_content, no_letters);
