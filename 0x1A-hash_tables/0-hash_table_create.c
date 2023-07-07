33cb58916d996d37e8cd014e1720794e731f5e9d
Fadyy22 <fadyalaa441@gmail.com>
2023-07-07 15:19:36 +0300

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,20 +12,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int counter;
 	hash_table_t *ht;
-	hash_node_t **array;
 
-	ht = malloc(sizeof(hash_node_t));
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
 
 	for (counter = 0; counter < size; counter++)
-		array[counter] = NULL;
+		ht->array[counter] = NULL;
 
-	ht->size = size;
-	ht->array = array;
 	return (ht);
 }
