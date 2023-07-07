56631461ae38eb4f2988c6aec5a94d08c5c30621
Fadyy22 <fadyalaa441@gmail.com>
2023-07-07 15:12:58 +0300

    >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ *
+ * @size: size of the array
+ *
+ * Return: pointer to the newly created hash table
+ * If something went wrong, return NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int counter;
+	hash_table_t *ht;
+
+	ht = malloc(sizeof(hash_node_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+
+	for (counter = 0; counter < size; counter++)
+		ht->array[counter] = NULL;
+
+	return (ht);
+}
