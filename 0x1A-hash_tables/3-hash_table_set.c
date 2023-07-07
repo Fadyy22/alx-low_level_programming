d5c0c624febe6dc2d491a1f3390c76047ca92fd1
Fadyy22 <fadyalaa441@gmail.com>
2023-07-07 23:52:47 +0300

    >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,6 +16,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *new_node;
 
+	if (strcmp(key, "") == 0)
+		return (0);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -25,6 +28,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->next = NULL;
 
 	index = key_index((const unsigned char *)key, ht->size);
+	printf("%li\n", index);
 
 	if (ht->array[index] == NULL)
 	{
